b29f6d6889c35a895a2b16e7ed8ce2c85b897ce1
Artem Smirnov <artem.s13@gmail.com>
2024-10-21 14:19:51 +0300

    fix: linter formating

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -2,44 +2,48 @@ import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    const result = simpleCalculator({ a: 2, b: 3, action: Action.Add })
+    const result = simpleCalculator({ a: 2, b: 3, action: Action.Add });
 
-    expect(result).toBe(5)
+    expect(result).toBe(5);
   });
 
   test('should subtract two numbers', () => {
-    const result = simpleCalculator({ a: 8, b: 3, action: Action.Subtract })
+    const result = simpleCalculator({ a: 8, b: 3, action: Action.Subtract });
 
-    expect(result).toBe(5)
+    expect(result).toBe(5);
   });
 
   test('should multiply two numbers', () => {
-    const result = simpleCalculator({ a: 2, b: 3, action: Action.Multiply })
+    const result = simpleCalculator({ a: 2, b: 3, action: Action.Multiply });
 
-    expect(result).toBe(6)
+    expect(result).toBe(6);
   });
 
   test('should divide two numbers', () => {
-    const result = simpleCalculator({ a: 9, b: 3, action: Action.Divide })
+    const result = simpleCalculator({ a: 9, b: 3, action: Action.Divide });
 
-    expect(result).toBe(3)
+    expect(result).toBe(3);
   });
 
   test('should exponentiate two numbers', () => {
-    const result = simpleCalculator({ a: 3, b: 2, action: Action.Exponentiate })
+    const result = simpleCalculator({
+      a: 3,
+      b: 2,
+      action: Action.Exponentiate,
+    });
 
-    expect(result).toBe(9)
+    expect(result).toBe(9);
   });
 
   test('should return null for invalid action', () => {
-    const result = simpleCalculator({ a: 3, b: 3, action: '%' })
+    const result = simpleCalculator({ a: 3, b: 3, action: '%' });
 
-    expect(result).toBeNull()
+    expect(result).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    const result = simpleCalculator({ a: 3, b: '3', action: Action.Divide })
+    const result = simpleCalculator({ a: 3, b: '3', action: Action.Divide });
 
-    expect(result).toBeNull()
+    expect(result).toBeNull();
   });
 });
